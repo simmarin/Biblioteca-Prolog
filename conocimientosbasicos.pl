34c056517b68a04ffb24e922a3c20a71a8fe99ec
egaitan <a13491074@itmexicali.edu.mx>
2017-03-14 17:39:46 -0700

    conocimientos basicos

---
+++ conocimientosbasicos.pl
@@ -0,0 +1,11 @@
+maestro(jorgeatempa,programacionlogica).
+maestro(jorgeatempa,redesdecomputadoras).
+maestro(jorgeatempa,redesinalambricas).
+maestro(jorgesalgado,calculodiferencial).
+maestro(jorgesalgado,calculovectorial).
+maestro(jorgesalgado,ecuacionesdiferenciales).
+maestro(ortarendon,ecucacionesdiferenciales).
+maestro(ortarendon,algebralineal).
+
+
+imparte(X,Y):- maestro(X,Y).
