e4862137031c6de87c72b9fbed00c4f290b5e7af
egaitan <a13491074@itmexicali.edu.mx>
2017-03-14 19:53:15 -0700

    agrege libros que utiliza la materia a la base de conocimientos

--- conocimientosbasicos.pl
+++ conocimientosbasicos.pl
@@ -1,11 +1,31 @@
 maestro(jorgeatempa,programacionlogica).
 maestro(jorgeatempa,redesdecomputadoras).
-maestro(jorgeatempa,redesinalambricas).
+maestro(veronicaquintero,automatas).
 maestro(jorgesalgado,calculodiferencial).
+maestro(jorgesalgado,integral).
 maestro(jorgesalgado,calculovectorial).
 maestro(jorgesalgado,ecuacionesdiferenciales).
-maestro(ortarendon,ecucacionesdiferenciales).
+maestro(ortarendon,ecuacionesdiferenciales).
 maestro(ortarendon,algebralineal).
 
-
 imparte(X,Y):- maestro(X,Y).
+
+materia(programacionlogica,computacionalintelligence).
+materia(programacionlogica,simplylogical).
+materia(programacionlogica,introduccionahaskell).
+materia(redesdecomputadoras,introduccionalasredesdedatos).
+materia(redesdecomputadoras,comunicacionesdigitales).
+materia(redesdecomputadoras,transmisiondedatosyredesdecomunicacion).
+materia(automatas,teoriadeautomatasylenguajesformales).
+materia(automatas,teoriadelacomputacion).
+materia(calculodiferencial,matematicas1).
+materia(calculodiferencial,calculodelasvariables).
+materia(calculointegral,matematicas2).
+materia(calculointegral,matematicasavanzadasdeingenieria).
+materia(calculovectorial,matematicas3).
+materia(calculovectorial,analisisdefourier).
+materia(ecuacionesdiferenciales,matematicasavanzadasparaingenieria).
+materia(ecuacionesdiferenciales,ecuacionesdiferencialesconaplicacionmodelada).
+materia(algebralineal,baldor).
+
+libro(X,Y):- materia(X,Y).
