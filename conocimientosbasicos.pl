1c952ed99022631745d90c0aa953354183ad7e43
simmarin <s.mtz.marin@gmail.com>
2017-03-15 22:47:16 -0700

    se agregaron hechos para las caracteristicas de los libros

--- conocimientosbasicos.pl
+++ conocimientosbasicos.pl
@@ -19,6 +19,32 @@ materia(comnutacion).
 
 existeMateria(X):- materia(X).
 
+libro(computacional_intelligence).
+libro(simply_logical).
+libro(introduccion_a_haskell).
+libro(introduccion_a_las_redes_de_datos).
+libro(comunicaciones_digitales).
+libro(transmision_de_datos_y_redes_de_comunicacion).
+libro(teoria_de_automatas_y_lenguajes_formales).
+libro(teoria_de_lacomputacion).
+libro(matematicas_1).
+libro(calculo_de_las_variables).
+libro(matematicas_2).
+libro(matematicas_avanzadas_de_ingenieria).
+libro(matematicas_3).
+libro(analisis_de_fourier).
+libro(matematicas_avanzadas_para_ingenieria_v1).
+libro(ecuaciones_diferenciales_con_aplicacion_modelada).
+libro(baldor).
+libro(algebra_lineal_y_aplicaciones).
+libro(telecomunicaciones_y_redes).
+libro(redes_informaticas).
+libro(redes_wireless).
+libro(las_redes).
+libro(redes_cisco).
+
+existeLibro(X):- libro(X).
+
 
 maestroMateria(jorge_atempa,programacion_logica).
 maestroMateria(jorge_atempa,redes_de_computadoras).
@@ -63,31 +89,6 @@ materiaLibro(redes_inalambricas,las_redes).
 
 utilizalibro(X,Y):- materiaLibro(X,Y).
 
-libro(computacional_intelligence).
-libro(simply_logical).
-libro(introduccion_a_haskell).
-libro(introduccion_a_las_redes_de_datos).
-libro(comunicaciones_digitales).
-libro(transmision_de_datos_y_redes_de_comunicacion).
-libro(teoria_de_automatas_y_lenguajes_formales).
-libro(teoria_de_lacomputacion).
-libro(matematicas_1).
-libro(calculo_de_las_variables).
-libro(matematicas_2).
-libro(matematicas_avanzadas_de_ingenieria).
-libro(matematicas_3).
-libro(analisis_de_fourier).
-libro(matematicas_avanzadas_para_ingenieria_v1).
-libro(ecuaciones_diferenciales_con_aplicacion_modelada).
-libro(baldor).
-libro(algebra_lineal_y_aplicaciones).
-libro(telecomunicaciones_y_redes).
-libro(redes_informaticas).
-libro(redes_wireless).
-libro(las_redes).
-libro(redes_cisco).
-
-existeLibro(X):- libro(X).
 
 maestromaterialibro(jorge_salgado,calculo_diferencial,matematicas_1).
 maestromaterialibro(jorge_salgado,calculo_diferencial,calculo_de_las_variables).
@@ -101,3 +102,15 @@ maestromaterialibro(mario_chong,redes_inalambricas,las_redes).
 maestromaterialibro(mario_chong,redes_inalambricas,redes_wireless).
 
 libroutiliza(X,Y,Z):-maestromaterialibro(X,Y,Z).
+
+
+clibro(matematicas_1,blanco,buho).
+clibro(calculo_de_las_variables,azul,bat).
+clibro(las_redes,blanco,lineas).
+clibro(redes_cisco,rojo,lineasydatos).
+clibro(computacional_intelligence,morado,cabezas).
+clibro(introduccion_a_haskell,gris,cuadroslateral).
+clibro(simply_logical,negro,lineas).
+clibro(redes_wireless,azul,simbolowifi).
+
+comoeslibro(X,Y,Z):-clibro(X,Y,Z).
